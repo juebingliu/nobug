d110144e1f6757837c8c7e0a7b609cce80b5ccf0
juebingliu <farewe1l2018x@gmail.com>
2019-05-15 19:51:47 +0800

    集成shiro

---
+++ src/main/java/com/dbpf/nobug/config/ShiroConfig.java
@@ -0,0 +1,56 @@
+package com.dbpf.nobug.config;
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.apache.shiro.mgt.SecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * @author juebing
+ * @version v1.0
+ * @date 2019/5/15 19:31
+ * @description
+ */
+@Configuration
+public class ShiroConfig {
+
+    @Bean(name = "shiroFilter")
+    public ShiroFilterFactoryBean shiroFilter(SecurityManager securityManager) {
+        ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
+        shiroFilterFactoryBean.setSecurityManager(securityManager);
+        shiroFilterFactoryBean.setLoginUrl("/login");
+        shiroFilterFactoryBean.setUnauthorizedUrl("/notRole");
+        Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
+        // <!-- authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问-->
+        filterChainDefinitionMap.put("/webjars/**", "anon");
+        filterChainDefinitionMap.put("/login", "anon");
+        filterChainDefinitionMap.put("/", "anon");
+        filterChainDefinitionMap.put("/front/**", "anon");
+        filterChainDefinitionMap.put("/api/**", "anon");
+
+        filterChainDefinitionMap.put("/admin/**", "authc");
+        filterChainDefinitionMap.put("/user/**", "authc");
+        //主要这行代码必须放在所有权限设置的最后，不然会导致所有 url 都被拦截 剩余的都需要认证
+        filterChainDefinitionMap.put("/**", "authc");
+        shiroFilterFactoryBean.setFilterChainDefinitionMap(filterChainDefinitionMap);
+        return shiroFilterFactoryBean;
+
+    }
+
+    @Bean
+    public SecurityManager securityManager() {
+        DefaultWebSecurityManager defaultSecurityManager = new DefaultWebSecurityManager();
+        defaultSecurityManager.setRealm(customRealm());
+        return defaultSecurityManager;
+    }
+
+    @Bean
+    public CustomRealm customRealm() {
+        CustomRealm customRealm = new CustomRealm();
+        return customRealm;
+    }
+}
